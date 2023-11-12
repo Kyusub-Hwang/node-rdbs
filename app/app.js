0ad78c41485c8971fbd31d2a7debc48d6f411eeb
Kyusub-Hwang <kyusub.hwang.dev@gmail.com>
2023-11-12 10:07:29 -0500

    log tracking implemented

--- app/app.js
+++ app/app.js
@@ -2,12 +2,18 @@
 
 //module
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const app = express();
 dotenv.config();
+
+const accessLogStream = require("./src/config/log")
 
 //Routing
 const home = require("./src/routes/home");
+
+ 
 
 //app setting
 app.set("views", "./src/views");
@@ -17,6 +23,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(morgan("common", {stream: accessLogStream}));
+
 app.use("/", home);
 
 module.exports = app;
---
+++ app/src/config/log.js
@@ -0,0 +1,7 @@
+
+const appRoot = require("app-root-path");
+const fs = require("fs");
+
+const accessLogStream = fs.createWriteStream(`${appRoot}/log/access.log`, { flags: 'a' })
+
+module.exports = accessLogStream;
