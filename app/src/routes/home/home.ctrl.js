cc7e7ad3164cca0bd4432b0834c8f0ec8cd48a64
Kyusub-Hwang <kyusub.hwang.dev@gmail.com>
2023-11-12 17:39:14 -0500

    HTTP response code updated

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,34 +5,47 @@ const logger = require("../../config/logger");
 
 const output = {
     hello: (req, res) => {
-        logger.info(`GET / 200 "Accessed to Homepage"`);
+        logger.info(`GET / 304 "Accessed to Homepage"`);
         res.render("home/index");
     },
     login: (req,res) => {
-        logger.info(`GET /login 200 "Accessed to Login"`);
+        logger.info(`GET /login 304 "Accessed to Login"`);
         res.render("home/login")
     },
     register: (req,res) => {
-        logger.info(`GET /register 200 "Accessed to Register"`);
+        logger.info(`GET /register 304 "Accessed to Register"`);
         res.render("home/register")
     }
 };
+
+const log = (response, url) => {
+    if (response.err) {logger.error(`${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`)}
+    else {logger.info(`${url.method} ${url.path} ${url.status} Response: ${response.success},msg: ${response.msg || ""}`)};
+}
 
 const process = {
     login: async (req,res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if (response.err) logger.error(`POST /login 200 Response: "success: ${response.success}, ${response.err}"`)
-        else logger.info(`POST /login 200 Response: "success: ${response.success},msg: ${response.msg}"`);
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/login",
+            status: response.err ? 400 : 200,
+        }
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 
     register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if (response.err) logger.error(`POST /register 200 Response: "success: ${response.success}, ${response.err}"`)
-        else logger.info(`POST /register 200 Response: "success: ${response.success},msg: ${response.msg}"`);
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/register",
+            status: response.err ? 500 : 201,
+        }
+        log(response, url);
+        return res.status(url.status).json(response);
     }
 };
 
