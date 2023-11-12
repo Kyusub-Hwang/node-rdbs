17ea5b37a076ef16613c837e9b80e603a915edbd
Kyusub-Hwang <kyusub.hwang.dev@gmail.com>
2023-11-12 15:02:42 -0500

    Log control (Winston) implemented

--- app/app.js
+++ app/app.js
@@ -3,16 +3,16 @@
 //module
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
 
 const app = express();
 dotenv.config();
 
-const accessLogStream = require("./src/config/log")
 
 //Routing
 const home = require("./src/routes/home");
 
+const logger = require("./src/config/logger");
+logger.log("info", "ACTIVITY LOGGED")
  
 
 //app setting
@@ -23,7 +23,6 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(morgan("common", {stream: accessLogStream}));
 
 app.use("/", home);
 
--- app/bin/www.js
+++ app/bin/www.js
@@ -2,7 +2,8 @@
 
 const app = require("../app");
 const PORT = process.env.PORT;
+const logger = require("../src/config/logger");
 
 app.listen(PORT, () => {
-    console.log("server running...");
-})
+    logger.info(`server running at port:${PORT}`);
+}) 
